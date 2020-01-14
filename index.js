c17013c5fff55d3576cff8605ba0243be68cf5ba
erri120 <erri120@protonmail.com>
2020-01-14 16:13:35 +0100

    CHANGELOG will also be exported on build

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
 const markdownpdf = require('markdown-pdf');
 const fs = require('fs');
 const path = require('path');
+const _ = require("underscore");
 
 const buildDir = path.join(__dirname, 'build');
-const inPath = path.join(__dirname, 'README.md');
-const outPath = path.join(buildDir, 'README.pdf');
 const cssPath = path.join(__dirname, 'css.css');
+
+const inPaths = {
+    readme: path.join(__dirname, 'README.md'),
+    changelog: path.join(__dirname, 'CHANGELOG.md')
+};
+
 
 if(!fs.existsSync(buildDir))
     fs.mkdirSync(buildDir);
 
-if(fs.existsSync(outPath))
-    fs.unlinkSync(outPath);
+_.map(inPaths, (cur) => {
+    const outPath = cur.replace(__dirname, buildDir).replace('.md', '.pdf');
+    if(fs.existsSync(outPath))
+        fs.unlinkSync(outPath);
 
-fs.createReadStream(inPath)
-  .pipe(markdownpdf({
-      cssPath: cssPath
-  }))
-  .pipe(fs.createWriteStream(outPath));
+    fs.createReadStream(cur)
+    .pipe(markdownpdf({
+        cssPath: cssPath
+    }))
+    .pipe(fs.createWriteStream(outPath));
+});
